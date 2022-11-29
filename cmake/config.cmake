1b2b917f6c8df3e0e7c6c53547609d60034e79be
Gautam Ghosh <ghosh@ins.uni-bonn.de>
2022-11-29 10:54:39 -0800

    Fixed minor typo for jansson check in cmake

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -101,7 +101,7 @@ set(CMAKE_REQUIRED_DEFINITIONS)
 
 # Check for ljansson
 check_include_file(jansson.h SC_HAVE_JSON_H)
-check_symbol_exists(json_integer math.h SC_HAVE_JSON_H)
+check_symbol_exists(json_integer jansson.h SC_HAVE_JSON)
 
 check_symbol_exists(fabs math.h SC_HAVE_FABS)
 
